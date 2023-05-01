e95e1cbe38f667b8b5bf64907f225a15758743e6
Mafalda Aires <116459746+mafaldaaires@users.noreply.github.com>
2023-05-01 23:56:20 +0100

    Add files via upload

---
+++ extract_musicalbums_info.py
@@ -0,0 +1,50 @@
+import requests
+import re
+import json
+from tqdm import tqdm
+
+# Metacritic API url
+url = "https://mcgqlapi.com/api"
+
+# Using readlines()
+albums_file = open('albums.txt', 'r')
+albums_lines = albums_file.readlines()
+
+albums = []
+
+# Obtain information using API
+for album_line in tqdm(albums_lines, desc='Querying MetacriticAPI...', total=len(albums_lines)):
+    try:
+        title, artist, genre, year, label, sales = re.findall('(.*) \((.*)\)', album_line)[0]
+
+        query = (
+          'query {',
+          '  album(input: {',
+          f'    title: "{title}",',
+          f'    artist: "{artist}",',
+          f'    genre: "{genre}",',
+          f'    year: "{year}",',
+          f'    label: "{label}",',
+          f'    sales: "{sales}"',
+          '  }) {',
+          '    title',
+          '    artist',
+          '    genre',
+          '    year',
+          '    label',
+          '    sales',
+          '  }',
+          '}'
+        )
+
+        query = '\n'.join(query)
+
+        response = requests.post(url=url, json={"query": query})
+        if response.status_code == 200:
+            albums.append(response.json())
+    except ValueError:
+        # Skip malformed line
+        continue
+
+with open('albums.json', 'w') as output_file:
+    output_file.write(json.dumps(albums, indent=4))
