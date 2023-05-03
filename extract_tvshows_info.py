3eab2d9c028d906a51a0660b53658eb7e195abeb
Mariana <126023917+marianaas8@users.noreply.github.com>
2023-05-03 22:31:12 +0100

    Update extract_tvshows_info.py

--- extract_tvshows_info.py
+++ extract_tvshows_info.py
@@ -7,37 +7,86 @@ from tqdm import tqdm
 url = "https://mcgqlapi.com/api"
 
 # Using readlines()
-tvshow_file = open('tv-shows.txt', 'r')
-tvshow_lines = tvshow_file.readlines()
+shows_file = open('tv-shows.txt', 'r')
+shows_lines = shows_file.readlines()
 
-tvshow = {}
+shows = {}
+elementos = []
+
+for show_line in shows_lines:
+    if (show_line[0] != '"'):
+        try:
+        
+            info = show_line.split(", ")
+
+            titulo, ano = info[0].split("(")
+            network = info[1].strip()
+
+            titulo = titulo.strip()
+            ano = ano.strip(")")
+
+            adaptation = "Original"
+            if info[-1].strip().endswith("Adaptation)"):
+                generos = [genero.strip() for genero in info[2:-3]]
+                produtora = info[-3].strip()
+                classificacao = info[-2].strip()
+                if info[-1].strip().endswith("Book Adaptation)"):
+                    adaptation = "Based on a book"
+                if info[-1].strip().endswith("Comic Book Adaptation)"):
+                    adaptation = "Based on a comic book"
+                if info[-1].strip().endswith("Film Adaptation)"):
+                    adaptation = "Based on a movie"
+            else:
+                generos = [genero.strip() for genero in info[2:-2]]
+                produtora = info[-2].strip()
+                classificacao = info[-1].strip()
+                classificacao = classificacao.strip(")")
+        except IndexError:
+            # Skip malformed line
+            continue
+        nova_string = f'"{titulo}" ({ano}) - {network} - {produtora} - {", ".join(generos)} - {classificacao} - {adaptation}'
+        
+    else:
+        info = show_line.split(" - ")
+        titulo_ano = info[0]
+        produtora = info[1]
+        genero = info[2]
+        classificacao = info[3]
+        adaptation = info[4].strip()
+        if adaptation == "Not a book adaptation":
+            adaptation = "Original"
+        titulo, ano = titulo_ano[1:-1].split(" (")
+        network = "None"
+
+        nova_string = f'"{titulo}" ({ano}) - {network} - {produtora} - {genero} - {classificacao} - {adaptation}'
+    elementos.append(nova_string)
 
 # Obtain information using API
-for tvshow_line in tvshow_lines:
-    
+for show_line in elementos:
+    lista = show_line.split(" - ")
+    titulo_ano = lista[0]
+    titulo, ano = titulo_ano.split(" (")
+    titulo = titulo.replace('"', '')
+    ano = ano.strip(")").split("-")
     try:
-        # split the string into its components
-        title, info = tvshow_line.split(" (")
-        info = info[:-2]  # remove the closing parenthesis
-        
-        # extract the relevant information from the components
-        elements = info.split(", ")
-        
-        # create a dictionary with the extracted information
+        # criar um dicionário com as informações obtidas
         show = {
-            "year": elements[0],
-            "network": elements[1],
-            "genre": elements[2],
-            "owner": elements[3],
-            "mode": elements[4]
+            "title": titulo,
+            "year": "-".join(ano),
+            "network": lista[1],
+            "studio": lista[2],
+            "genre": lista[3],
+            "rating": lista[4],
+            "adaptation": lista[5]
         }
-
-        # add the dictionary to a main dictionary
-        tvshow[title] = {"data": {"show": show}}
+        
+        if titulo not in shows:
+            # adicionar o dicionário ao dicionário principal
+            shows[titulo] = {"data": {"tv-show": show}}
 
     except IndexError:
-        # skip malformed line
+        # Skip malformed line
         continue
 
 with open('tv-shows.json', 'w') as output_file:
-    output_file.write(json.dumps(tvshow))
+    output_file.write(json.dumps(shows))
